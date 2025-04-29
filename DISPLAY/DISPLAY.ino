8a5b1bc7a0c8c399fe2f700a7aba0f3d7839d7b1
Rebeca Mendonça <beca.martins28@gmail.com>
2025-04-29 13:43:48 -0300

    first commit

---
+++ DISPLAY/DISPLAY.ino
@@ -0,0 +1,20 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(7,6,5,4,3,2);
+
+void setup() {
+  // 
+  lcd.begin(16,2);
+  lcd.clear();
+
+}
+
+void loop() {
+  // 
+  lcd.setCursor(3,0);
+  lcd.print("Rebeca");
+  lcd.setCursor(4,1);
+  lcd.print("Arduino");
+  delay(3000);
+
+}
